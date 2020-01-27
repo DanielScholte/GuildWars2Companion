1488a9d74587917b2cea044af642941c708b4a09
danielscholte <danielscholte@live.nl>
2020-01-27 17:34:58 +0100

    merge duplicates

--- lib/repositories/event.dart
+++ lib/repositories/event.dart
@@ -39,9 +39,28 @@ class EventRepository {
       }
 
       event.segments.forEach((segment) {
+        if (segment.times.isEmpty) {
+          return;
+        }
+
+        List<MetaEventSegment> duplicates = event.segments.where((s) => s.name == segment.name && s != segment).toList();
+
+        if (duplicates.isNotEmpty) {
+          duplicates.forEach((d) {
+            segment.times.addAll(d.times);
+            d.times = [];
+          });
+
+          segment.times.sort((a, b) => a.compareTo(b));
+        }
+
         segment.time = segment.times
           .firstWhere((t) => t.add(segment.duration).isAfter(now), orElse: () => segment.times[0].add(Duration(days: 1)));
       });
+
+      event.segments.removeWhere((s) => s.times.isEmpty || s.time == null);
+
+      event.segments.sort((a, b) => a.time.compareTo(b.time));
     });
 
     return events;
