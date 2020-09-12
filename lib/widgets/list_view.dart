802c9bbfa2298fdf4d0cb1f59f1650dae042751f
DanielScholte <danielscholte@live.nl>
2020-09-12 13:54:10 +0200

    Adjusted ListView bottom padding

--- lib/widgets/list_view.dart
+++ lib/widgets/list_view.dart
@@ -9,14 +9,17 @@ class CompanionListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    EdgeInsets defaultPadding = MediaQuery
+      .of(context)
+      .padding;
+
     return ListView(
-      padding: MediaQuery
-        .of(context)
-        .padding
+      padding: defaultPadding
         .copyWith(
           top: 6.0,
           left: 0.0,
           right: 0.0,
+          bottom: defaultPadding.bottom > 6.0 ? defaultPadding.bottom : 6.0
         ),
       children: children,
     );
