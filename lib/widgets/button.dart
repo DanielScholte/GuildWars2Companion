318dc495c2aa5ae6278554b53e3941b9acf8f39c
DanielScholte <danielscholte@live.nl>
2020-10-01 15:14:11 +0200

    Reworked button shadows->elevation

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class CompanionButton extends StatelessWidget {
+class CompanionButton extends StatefulWidget {
   final bool loading;
   final Widget leading;
   final Widget trailing;
@@ -32,42 +32,71 @@ class CompanionButton extends StatelessWidget {
   });
 
   @override
+  _CompanionButtonState createState() => _CompanionButtonState();
+}
+
+class _CompanionButtonState extends State<CompanionButton> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animationTween;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+    _animationTween = Tween(begin: 2.0, end: 8.0).animate(_animationController);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: height,
-      decoration: BoxDecoration(
-        color: Theme.of(context).brightness == Brightness.light ? color : Color(0xFF323232),
-        borderRadius: BorderRadius.circular(12.0),
-        boxShadow: [
-          if (Theme.of(context).brightness == Brightness.light)
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 8.0,
-            ),
-        ],
-      ),
-      margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(12.0),
-        child: wrapper != null ? wrapper(context, _buildBody(context)) : _buildBody(context),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: AnimatedBuilder(
+        animation: _animationTween,
+        builder: (context, child) {
+          return Material(
+            elevation: _animationTween.value,
+            borderRadius: BorderRadius.circular(13.0),
+            child: child,
+            shadowColor: Colors.black87,
+          );
+        },
+        child: Container(
+          width: double.infinity,
+          height: widget.height,
+          decoration: BoxDecoration(
+            color: Theme.of(context).brightness == Brightness.light ? widget.color : Color(0xFF323232),
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12.0),
+            child: widget.wrapper != null ? widget.wrapper(context, _buildBody(context)) : _buildBody(context),
+          ),
+        ),
       ),
     );
   }
 
   Widget _buildBody(BuildContext context) {
-    return onTap != null ? Material(
+    return widget.onTap != null ? Material(
       color: Colors.transparent,
       child: InkWell(
         splashColor: Colors.black12,
         highlightColor: Colors.black12,
-        onTap: () => onTap(),
+        onTap: () => widget.onTap(),
+        onTapDown: (_) => _animationController.forward(),
+        onFocusChange: (value) => !value ? _animationController.reverse() : _animationController.animateTo(.5),
+        onHover: (value) => !value ? _animationController.reverse() : _animationController.animateTo(.5),
+        onHighlightChanged: (value) => !value ? _animationController.reverse() : _animationController.forward(),
         child: Row(
           children: <Widget>[
             _buildLeadingContainer(context),
             _buildTitle(context),
-            if (trailing != null)
-              trailing,
+            if (widget.trailing != null)
+              widget.trailing,
             _buildArrow(context)
           ],
         ),
@@ -76,20 +105,20 @@ class CompanionButton extends StatelessWidget {
       children: <Widget>[
         _buildLeadingContainer(context),
         _buildTitle(context),
-        if (trailing != null)
-          trailing,
+        if (widget.trailing != null)
+          widget.trailing,
       ],
     );
   }
 
   Widget _buildLeadingContainer(BuildContext context) {
     return Container(
-      width: height == null ? 80.0 : height,
-      height: height,
+      width: widget.height == null ? 80.0 : widget.height,
+      height: widget.height,
       color: Colors.black12,
       margin: EdgeInsets.only(right: 12.0),
       alignment: Alignment.center,
-      child: this.loading ?
+      child: this.widget.loading ?
         Theme(
           data: Theme.of(context).copyWith(accentColor: Colors.white),
           child: CircularProgressIndicator(),
@@ -98,28 +127,28 @@ class CompanionButton extends StatelessWidget {
   }
 
   Widget _buildLeading(BuildContext context) {
-    if (hero != null) {
+    if (widget.hero != null) {
       return Hero(
-        tag: hero,
+        tag: widget.hero,
         child: ClipRRect(
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(11.0),
             bottomLeft: Radius.circular(11.0),
           ),
-          child: leading,
+          child: widget.leading,
         ),
       );
     }
 
-    return leading;
+    return widget.leading;
   }
 
   Widget _buildTitle(BuildContext context) {
     List<Widget> titles = [
       Text(
-        this.title,
+        this.widget.title,
         style: TextStyle(
-          color: Theme.of(context).brightness == Brightness.light ? foregroundColor : Colors.white,
+          color: Theme.of(context).brightness == Brightness.light ? widget.foregroundColor : Colors.white,
           fontSize: 18.0,
           fontWeight: FontWeight.w500
         ),
@@ -128,17 +157,17 @@ class CompanionButton extends StatelessWidget {
       ),
     ];
 
-    if (this.subtitleWidgets != null) {
-      titles.addAll(this.subtitleWidgets);
+    if (this.widget.subtitleWidgets != null) {
+      titles.addAll(this.widget.subtitleWidgets);
     }
 
-    if (this.subtitles != null) {
+    if (this.widget.subtitles != null) {
       titles.addAll(
-        this.subtitles.map(
+        this.widget.subtitles.map(
           (s) => Text(
             s,
             style: TextStyle(
-              color: Theme.of(context).brightness == Brightness.light ? foregroundColor : Colors.white,
+              color: Theme.of(context).brightness == Brightness.light ? widget.foregroundColor : Colors.white,
               fontSize: 16.0,
             ),
           )
@@ -160,7 +189,7 @@ class CompanionButton extends StatelessWidget {
       padding: EdgeInsets.all(8.0),
       child: Icon(
         FontAwesomeIcons.chevronRight,
-        color: Theme.of(context).brightness == Brightness.light ? foregroundColor : Colors.white,
+        color: Theme.of(context).brightness == Brightness.light ? widget.foregroundColor : Colors.white,
         size: 18.0,
       ),
     );
